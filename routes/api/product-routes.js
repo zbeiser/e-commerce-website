5f0d75fb04a129a7c2f94819b2dfbb4ac5771208
Zach Beiser <zbeiser@gmail.com>
2023-01-26 13:59:31 -0800

    added GET '/' routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,13 +4,26 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{
+        model: Category
+      },
+      {
+        model: Tag, through: ProductTag
+      }]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
@@ -89,7 +102,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
 });
 
