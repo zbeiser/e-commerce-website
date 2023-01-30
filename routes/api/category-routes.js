6be4b9db106d5ca37c64146e14c631c8bf7e44e6
Zach Beiser <zbeiser@gmail.com>
2023-01-30 10:40:45 -0800

    added put '/:id' routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,6 +49,23 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedCategory) => {
+      res.json(updatedCategory);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
 });
 
 router.delete('/:id', async (req, res) => {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -49,6 +49,23 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedTag) => {
+      res.json(updatedTag);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
 });
 
 router.delete('/:id', async (req, res) => {
